43c3e069b427bc182156eb4d1ff7e91bde650409
Harshith <harshithdhara291@gmail.com>
2023-04-26 12:56:51 +0530

    final commit

--- src/components/WinOrLoseCard/index.js
+++ src/components/WinOrLoseCard/index.js
@@ -1,13 +1,14 @@
 import Fade from 'react-reveal/Fade';
 import './index.css'
 
-const LOSE_IMAGE = 'https://assets.ccbp.in/frontend/react-js/lose-game-img.png'
-const WON_IMAGE = 'https://assets.ccbp.in/frontend/react-js/won-game-img.png'
+const LOSE_IMAGE = 'https://res.cloudinary.com/di4qjlwyr/image/upload/v1682491937/483-4831080_very-sad-to-hart-emojis-icon-for-face-removebg-preview_e7bub2.png'
+const WON_IMAGE = 'https://res.cloudinary.com/di4qjlwyr/image/upload/v1682491589/istockphoto-544662460-612x612-removebg-preview_dhqxkw.png'
 
 const WinOrLoseCard = props => {
   const {isWon, onClickPlayAgain, score} = props
   const imageUrl = isWon ? WON_IMAGE : LOSE_IMAGE
   const gameStatus = isWon ? 'You Won' : 'You Lose'
+  const approClassName = isWon ? 'won-image' : 'lose-image'
   const scoreLabel = isWon ? 'Best Score' : 'Score'
 
   return (
@@ -26,9 +27,11 @@ const WinOrLoseCard = props => {
         </button>
       </div>
       </Fade>
+      <Fade right>
       <div className="image-section">
-        <img className="win-or-lose-image" src={imageUrl} alt="win or lose" />
+        <img className={approClassName} src={imageUrl} alt="win or lose" />
       </div>
+      </Fade>
     </div>
   )
 }
